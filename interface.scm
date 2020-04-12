fd75c1ca62a443193886bf6f03153ddc29f5dbdf
Aidan Lavis <alavis@vassar.edu>
2020-04-11 22:13:50 -0400

    interface updated

--- interface.scm
+++ interface.scm
@@ -5,19 +5,39 @@
 ;;  - Setting a word
 ;;  - Guessing a letter
 ;;  - Knowing if you've won, lost, or need to keep playing
+;;  - Knowing what letters you've gotten
+;;  - Knowing how many mistakes you've made
 
 (load "main.scm")
 
-(define state (init-state '()))
+(define game (init-state '()))
 
-(define set-state! ;; reset state with a particular word
+(define set-state! ;; reset game with a particular word
   (lambda (word)
-    (set! state (init-state word))))
+    (set! game (init-state word))))
+
+;; helper function for guess!
+(define get-letters
+  (lambda (state)
+    (define rm
+      (lambda (l1 l2)
+	(cond ((null? l1) l1)
+	      ((= (car l2) 1)
+	       (cons (first l1) (rm (rest l1) (rest l2))))
+	      (else
+		(cons '_ (rm (rest l1) (rest l2)))))))
+    (rm (get-wordlist state) (get-hitlist state))))
 
 (define guess!
   (lambda (letter)
-    (set! state (guess letter state))
-    (cond ((lost? state) 0) ;; 0 == lost
-	  ((won?  state) 1) ;; 1 == won
-	  (else          2) ;; 2 == still playing
-	  )))
+    (set! game (guess letter game))
+    (list
+      ;; won/lost/keep playing
+      (cond ((lost? game) 0) ;; 0 == lost
+	    ((won?  game) 1) ;; 1 == won
+	    (else          2) ;; 2 == still playing
+	    )
+      ;; what letters you've gotten
+      (get-letters game)
+      ;; how many mistakes you've made
+      (get-mistakes game))))
